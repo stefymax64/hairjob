5f1cfcdba65a67e48c4f023416d69363022ce563
stefymax64 <50297958+stefymax64@users.noreply.github.com>
2020-06-08 23:57:52 +0200

    Iteration 5_0

---
+++ public/js/carousel.js
@@ -0,0 +1,2 @@
+// Carousel Auto-Cycle
+
--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller\Security;
 
+use App\Entity\User;
 use App\Form\RegistrationType;
+use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends AbstractController
 {
@@ -31,5 +36,35 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \Exception(('Will be intercepted before getting here '));
+    }
+
+    /**
+     * @Route("/register", name="app_registrer")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardAuthenticatorHandler, LoginFormAuthenticator $formAuthenticator)
+    {
+        if ($request->isMethod('POST'))
+        {
+            $user = new User();
+            $user->setEmail($request->request->get('email'));
+            $user->setFirstName('Mystery');
+            $user->setPassword($passwordEncoder->encodePassword(
+                $user,
+                $request->request->get('password')
+            ));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $guardAuthenticatorHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $formAuthenticator,
+                'main'
+            );
+        }
+
+        return $this->render('security/register.html.twig');
     }
 }
